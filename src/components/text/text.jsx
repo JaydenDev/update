25b71e1b5c242317a0db1e125afa279f9a505418
JaydenDev <willsjayden2@gmail.com>
2022-06-30 13:55:32 -0500

    feat: add increasing progress %, with randomized increase rate and increase value

--- src/components/text/text.jsx
+++ src/components/text/text.jsx
@@ -1,14 +1,26 @@
 import '../../App.css';
 const React = require('react');
 
+var value = '10';
+var reset = Math.floor(Math.random() * (24000 - 3000 + 1)) + 3000;
+
 class Text extends React.Component {
     render() {
+        setInterval(() => {
+            var inc = Math.floor(Math.random() * 5) + 1;
+            if (value >= '100') {
+                value = '0';
+            }
+            value = parseInt(value) + inc;
+            this.forceUpdate();
+        }, reset);
         return (
         <div className="text">
-            <p>You're 80% there.</p>
+            <p>You're {value}% there.</p>
             <p>Please keep your computer on.</p>
         </div>
         );
     }
 }
+
 export default Text;
